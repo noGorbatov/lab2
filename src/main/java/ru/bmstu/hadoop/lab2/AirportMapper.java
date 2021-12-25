202920c3c2aa3c3010aac9e46cc3034e8ea9b1d4
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 06:49:40 +0300

    Scripted auto-commit on change (2021-12-25 06:49:40) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
@@ -8,6 +8,9 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, CompositeKeyComparable, Text> {
+    final String stripChars = "\"";
+    final String commaSplitter = ",";
+    final int RECORD_NUMBER = 2;
     @Override
     protected void map(LongWritable k, Text line, Context ctx) throws
             IOException, InterruptedException {
@@ -15,7 +18,9 @@ public class AirportMapper extends Mapper<LongWritable, Text, CompositeKeyCompar
             return;
         }
 
-        
+        String record = StringUtils.strip(line.toString(), stripChars);
+
+        String records[] = StringUtils.split(record, commaSplitter, RECORD_NUMBER);
 
     }
 }
