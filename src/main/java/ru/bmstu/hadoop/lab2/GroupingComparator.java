e12b7be27b7ef60649083837e27d6a076795585a
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 07:30:17 +0300

    Scripted auto-commit on change (2021-12-25 07:30:17) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/GroupingComparator.java
+++ src/main/java/ru/bmstu/hadoop/lab2/GroupingComparator.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class GroupingComparator extends WritableComparator {
+    public 
     @Override
     public int compare(WritableComparable o1, WritableComparable o2) {
         CompositeKeyComparable k1 = (CompositeKeyComparable) o1;
