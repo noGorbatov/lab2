4778a5e61878bfdf4d3ff748e0d46bc2d0f538d4
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 06:33:06 +0300

    Scripted auto-commit on change (2021-12-25 06:33:06) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportWritable.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportWritable.java
@@ -2,6 +2,19 @@ package ru.bmstu.hadoop.lab2;
 
 import org.apache.hadoop.io.Writable;
 
-public class AirportComparable implements Writable {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
+public class AirportWritable implements Writable {
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
