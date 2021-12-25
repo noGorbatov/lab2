94b3939abf73a4391ac52d0ddc28a65832348d89
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 06:38:16 +0300

    Scripted auto-commit on change (2021-12-25 06:38:16) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/CompositeKeyComparable.java
+++ src/main/java/ru/bmstu/hadoop/lab2/CompositeKeyComparable.java
@@ -2,6 +2,24 @@ package ru.bmstu.hadoop.lab2;
 
 import org.apache.hadoop.io.WritableComparable;
 
-public class CompositeKeyComparable implements WritableComparable<> {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
+public class CompositeKeyComparable implements WritableComparable {
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
