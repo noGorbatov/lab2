d0019b88e8ef228378120696b6fcb4b4e5171144
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 07:18:58 +0300

    Scripted auto-commit on change (2021-12-25 07:18:58) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/TableJoinerApp.java
+++ src/main/java/ru/bmstu/hadoop/lab2/TableJoinerApp.java
@@ -11,7 +11,7 @@ public class TableJoinerApp {
 
     public static void main(String[] args) throws
             IOException, InterruptedException, ClassNotFoundException {
-        if (args.length != 2) {
+        if (args.length != 3) {
             System.err.println("Usage:");
             System.exit(-1);
         }
