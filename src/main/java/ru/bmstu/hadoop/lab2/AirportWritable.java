ec27b621a4f060ef134dd63c3101ec661d6a6165
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 06:33:47 +0300

    Scripted auto-commit on change (2021-12-25 06:33:47) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportWritable.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportWritable.java
@@ -1,5 +1,6 @@
 package ru.bmstu.hadoop.lab2;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 
 import java.io.DataInput;
@@ -7,14 +8,23 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class AirportWritable implements Writable {
-
+    private Text name;
+    public AirportWritable() {
+        name = new Text();
+    }
+    public AirportWritable(Text airportName) {
+        name = airportName;
+    }
+    public void set(Text airportName) {
+        name = airportName;
+    }
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-
+        name.write(dataOutput);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-
+        name.readFields(dataInput);
     }
 }
