e0abb0932198602cffe9b1d4e9f930051077dc02
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 06:48:25 +0300

    Scripted auto-commit on change (2021-12-25 06:48:25) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
@@ -1,4 +1,9 @@
 package ru.bmstu.hadoop.lab2;
 
-public class AirportMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AirportMapper extends Mapper<LongWritable, Text, CompositeKeyComparable, Text> {
+    
 }
