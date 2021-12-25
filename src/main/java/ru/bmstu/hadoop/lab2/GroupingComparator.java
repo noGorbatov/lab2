48bc292d72a8966fb9e7a2b48c846b835282036a
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 08:18:25 +0300

    Scripted auto-commit on change (2021-12-25 08:18:25) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/GroupingComparator.java
+++ src/main/java/ru/bmstu/hadoop/lab2/GroupingComparator.java
@@ -11,8 +11,9 @@ public class GroupingComparator extends WritableComparator {
     public int compare(WritableComparable o1, WritableComparable o2) {
         CompositeKeyComparable k1 = (CompositeKeyComparable) o1;
         CompositeKeyComparable k2 = (CompositeKeyComparable) o2;
-
-//        return k1.getAirportId() - k2.getAirportId();
-        return -1;
+        int id1 = k1.getAirportId();
+        int id2 = k2.getAirportId();
+        return id1 < id2 ? -1 : (id1 == id2 ? 0 : 1);
+//        return -1;
     }
 }
