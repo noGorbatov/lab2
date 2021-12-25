f2f8e81d9e79446a6895bb3de05ff0575924f754
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 07:03:59 +0300

    Scripted auto-commit on change (2021-12-25 07:03:59) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportFlightPartitioner.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportFlightPartitioner.java
@@ -1,4 +1,12 @@
 package ru.bmstu.hadoop.lab2;
 
-public class AirportFlightPartitioner {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AirportFlightPartitioner extends Partitioner<CompositeKeyComparable, Text> {
+
+    @Override
+    public int getPartition(CompositeKeyComparable compositeKeyComparable, Text text, int i) {
+        return 0;
+    }
 }
