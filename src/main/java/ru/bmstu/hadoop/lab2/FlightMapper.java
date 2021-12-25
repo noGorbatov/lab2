bd2af0b9b209325cfdb8d9f094119fd40f06f506
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 06:59:06 +0300

    Scripted auto-commit on change (2021-12-25 06:59:06) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/hadoop/lab2/FlightMapper.java
@@ -1,6 +1,5 @@
 package ru.bmstu.hadoop.lab2;
 
-import com.sun.org.apache.xpath.internal.operations.String;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -12,8 +11,6 @@ import java.util.ArrayList;
 public class FlightMapper extends Mapper<LongWritable, Text, CompositeKeyComparable, Text> {
     static final int DELAY_INDEX = 17;
     static final int AIRPORT_INDEX = 14;
-    static final String COMMA_SPLITTER = ",";
-    static final String STRIP_CHARS = "\"";
     @Override
     protected void map(LongWritable k, Text line, Context ctx) throws
             IOException, InterruptedException {
@@ -22,17 +19,12 @@ public class FlightMapper extends Mapper<LongWritable, Text, CompositeKeyCompara
         }
 
         String records[] = split(line.toString());
-        if (records.length != 2) {
-            return;
-        }
-        for (int i = 0; i < records.length; i++) {
-            records[i] = StringUtils.strip(records[i], STRIP_CHARS);
-        }
+        if ()
     }
 
     static private String[] split(String line) {
         ArrayList<String> records = new ArrayList<>();
-        int lineLen = line.length;
+        int lineLen = line.length();
         StringBuilder currentStr = new StringBuilder();
         for (int i = 0; i < lineLen; i++) {
             char c = line.charAt(i);
@@ -40,7 +32,10 @@ public class FlightMapper extends Mapper<LongWritable, Text, CompositeKeyCompara
                 case '"':
                     break;
                 case ',':
-
+                    records.add(currentStr.toString());
+                    currentStr = new StringBuilder();
+                    break;
+                default:
                     currentStr.append(c);
             }
         }
