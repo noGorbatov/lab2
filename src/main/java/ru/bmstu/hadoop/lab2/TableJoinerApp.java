2ce6624c1bf0eb1b0c30982143d6eacfbd95dee2
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 07:18:55 +0300

    Scripted auto-commit on change (2021-12-25 07:18:55) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/TableJoinerApp.java
+++ src/main/java/ru/bmstu/hadoop/lab2/TableJoinerApp.java
@@ -1,7 +1,7 @@
 package ru.bmstu.hadoop.lab2;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
@@ -16,7 +16,11 @@ public class TableJoinerApp {
             System.exit(-1);
         }
         Job job = Job.getInstance();
+        Path airportPath = new Path(args[0]);
+        Path flightsPath = new Path(args[1]);
+        Path outPath = new Path(args[2]);
         job.setJarByClass(TableJoinerApp.class);
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
+        MultipleInputs.addInputPath(job, , TextInputFormat.class, AirportMapper.class);
+        MultipleInputs
     }
 }
