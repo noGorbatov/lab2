81de84d20aee234a63068ce1beb05f11a69894fb
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 07:05:38 +0300

    Scripted auto-commit on change (2021-12-25 07:05:38) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/JoinTableReducer.java
+++ src/main/java/ru/bmstu/hadoop/lab2/JoinTableReducer.java
@@ -3,10 +3,18 @@ package ru.bmstu.hadoop.lab2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+import java.util.Iterator;
+
 
 public class JoinTableReducer extends Reducer<CompositeKeyComparable, Text, Text, Text> {
     @Override
-    protected void reduce(CompositeKeyComparable key, Iterable<Text> values, Context ctx) {
-        
+    protected void reduce(CompositeKeyComparable key, Iterable<Text> values, Context ctx) throws
+            IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        String airportName = iter.next().toString();
+        while (iter.hasNext()) {
+
+        }
     }
 }
