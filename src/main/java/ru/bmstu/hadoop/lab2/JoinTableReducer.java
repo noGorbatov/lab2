7ca32293005d65de353614e957d81d30d47223ef
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 08:22:54 +0300

    Scripted auto-commit on change (2021-12-25 08:22:54) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/JoinTableReducer.java
+++ src/main/java/ru/bmstu/hadoop/lab2/JoinTableReducer.java
@@ -13,7 +13,7 @@ public class JoinTableReducer extends Reducer<CompositeKeyComparable, Text, Text
     protected void reduce(CompositeKeyComparable key, Iterable<Text> values, Context ctx) throws
             IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
-        Text airportName = iter.next();
+        String airportName = iter.next().toString();
         double min = Integer.MAX_VALUE, sum = 0, max = 0;
         int n = 0;
         int i = 0;
@@ -46,24 +46,24 @@ public class JoinTableReducer extends Reducer<CompositeKeyComparable, Text, Text
 //                v = it.next().toString();
 //            }
 
-            ctx.write(airportName, new Text(value + " i = " + i + " n = " + n + " " + key.toString()));
-        } else {
-            boolean diff = false;
-            for (int j = 0; j < keys.size(); j++) {
-                for (int k = 0; k < keys.size(); k++) {
-                    if (j == k) {
-                        continue;
-                    }
-                    if (keys.get(j).getAirportId() != keys.get(k).getAirportId()) {
-                        diff = true;
-                    }
-                }
-            }
-            if (diff) {
-                ctx.write(airportName, new Text(key.toString() + " diff = true"));
-            } else {
-                ctx.write(airportName, new Text(key.toString() + " n = " + n));
-            }
+            ctx.write(new Text(airportName), new Text(value + " i = " + i + " n = " + n + " " + key.toString()));
+//        } else {
+//            boolean diff = false;
+//            for (int j = 0; j < keys.size(); j++) {
+//                for (int k = 0; k < keys.size(); k++) {
+//                    if (j == k) {
+//                        continue;
+//                    }
+//                    if (keys.get(j).getAirportId() != keys.get(k).getAirportId()) {
+//                        diff = true;
+//                    }
+//                }
+//            }
+//            if (diff) {
+//                ctx.write(airportName, new Text(key.toString() + " diff = true"));
+//            } else {
+//                ctx.write(airportName, new Text(key.toString() + " n = " + n));
+//            }
         }
     }
 }
