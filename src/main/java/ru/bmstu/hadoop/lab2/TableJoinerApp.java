04185dc41f3bc66b5e892dc4888081f374f04086
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 07:13:04 +0300

    Scripted auto-commit on change (2021-12-25 07:13:04) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/TableJoinerApp.java
+++ src/main/java/ru/bmstu/hadoop/lab2/TableJoinerApp.java
@@ -1,10 +1,14 @@
 package ru.bmstu.hadoop.lab2;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+
+import java.io.IOException;
 
 public class TableJoinerApp {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws
+            IOException, InterruptedException{
+        Job job = Job.getInstance();
     }
 }
