1e9e8d08d04e506c4c6f992be55f81665b70b157
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 06:48:40 +0300

    Scripted auto-commit on change (2021-12-25 06:48:40) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportMapper.java
@@ -4,6 +4,12 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, CompositeKeyComparable, Text> {
-    
+    @Override
+    protected void map(LongWritable k, Text line, Context ctx) throws
+            IOException, InterruptedException {
+
+    }
 }
